e423adc25014addb7317b6175abb0c59ee7043af
HyperPaint <dioman172@yandex.ru>
2023-07-28 21:11:20 +1000

    Сборка wordpress v3
    + Библиотека
    + Привёл в порядок Jenkinsfile

--- src/hyperpaint/util/DockerCompose.groovy
+++ src/hyperpaint/util/DockerCompose.groovy
@@ -6,50 +6,50 @@ final class DockerCompose {
     /* Стандартные функции */
 
     static String config(String projectDirectory) {
-        return Shell.executeWithOutput("docker compose --project-directory '${projectDirectory}' config")
+        return Shell.shWithOutput("docker compose --project-directory '${projectDirectory}' config")
     }
 
     static boolean create(String projectDirectory) {
-        return Shell.executeWithStatus("docker compose --project-directory '${projectDirectory}' create")
+        return Shell.shWithStatus("docker compose --project-directory '${projectDirectory}' create")
     }
 
     static boolean down(String projectDirectory) {
-        return Shell.executeWithStatus("docker compose --project-directory '${projectDirectory}' down")
+        return Shell.shWithStatus("docker compose --project-directory '${projectDirectory}' down")
     }
 
     static String logs(String projectDirectory) {
-        return Shell.executeWithOutput("docker compose --project-directory '${projectDirectory}' logs")
+        return Shell.shWithOutput("docker compose --project-directory '${projectDirectory}' logs")
     }
 
     static String ps(String projectDirectory, boolean q = false) {
         if (q) {
-            return Shell.executeWithOutput("docker compose --project-directory '${projectDirectory}' ps -qa")
+            return Shell.shWithOutput("docker compose --project-directory '${projectDirectory}' ps -qa")
         } else {
-            return Shell.executeWithOutput("docker compose --project-directory '${projectDirectory}' ps -a")
+            return Shell.shWithOutput("docker compose --project-directory '${projectDirectory}' ps -a")
         }
     }
 
     static boolean restart(String projectDirectory) {
-        return Shell.executeWithStatus("docker compose --project-directory '${projectDirectory}' restart")
+        return Shell.shWithStatus("docker compose --project-directory '${projectDirectory}' restart")
     }
 
     static boolean rm(String projectDirectory) {
-        return Shell.executeWithStatus("docker compose --project-directory '${projectDirectory}' rm")
+        return Shell.shWithStatus("docker compose --project-directory '${projectDirectory}' rm")
     }
 
     static boolean start(String projectDirectory) {
-        return Shell.executeWithStatus("docker compose --project-directory '${projectDirectory}' start")
+        return Shell.shWithStatus("docker compose --project-directory '${projectDirectory}' start")
     }
 
     static boolean stop(String projectDirectory) {
-        return Shell.executeWithStatus("docker compose --project-directory '${projectDirectory}' stop")
+        return Shell.shWithStatus("docker compose --project-directory '${projectDirectory}' stop")
     }
 
     static boolean up(String projectDirectory) {
-        return Shell.executeWithStatus("docker compose --project-directory '${projectDirectory}' up -d")
+        return Shell.shWithStatus("docker compose --project-directory '${projectDirectory}' up -d")
     }
 
     static String version() {
-        return Shell.executeWithOutput("docker compose version")
+        return Shell.shWithOutput("docker compose version")
     }
 }
