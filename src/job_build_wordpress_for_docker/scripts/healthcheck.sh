0be10a28aa11124dd89208440466a11e94328866
HyperPaint <dioman172@yandex.ru>
2023-08-05 20:04:48 +1000

    Сборка WordPress v3.5.8
    = Ничего

--- src/job_build_wordpress_for_docker/scripts/healthcheck.sh
+++ src/job_build_wordpress_for_docker/scripts/healthcheck.sh
@@ -11,13 +11,14 @@ error() {
 }
 
 healthcheck() {
-  return "$(curl -sf http://localhost:80)"
+  curl -f http://localhost:80
+  return $?
 }
 
 if healthcheck; then
-    log "Healthcheck OK"
-    exit 0
-  else
-    error "Healthcheck ERROR"
-    exit 1
-  fi
+  log "Healthcheck OK"
+  exit 0
+else
+  error "Healthcheck ERROR"
+  exit 1
+fi
