aa81f75ac278d7a418cea8acd6c041d699c3deb6
HyperPaint <dioman172@yandex.ru>
2023-09-03 13:28:08 +1000

    comment deletion

--- src/job_build_for_docker/nextcloud/pre_configure.sh
+++ src/job_build_for_docker/nextcloud/pre_configure.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
-for file in nextcloud/themes/*; do
-  if [ ! "$file" = "nextcloud/themes/index.php" ]; then
-    rm -rf "$file"
-    if [ $? ]; then
-      echo "Removed $file"
-    else
-      echo "Can't remove $file"
-      exit 1
-    fi
-  fi
-done
+#for file in nextcloud/themes/*; do
+#  if [ ! "$file" = "nextcloud/themes/index.php" ]; then
+#    rm -rf "$file"
+#    if [ $? ]; then
+#      echo "Removed $file"
+#    else
+#      echo "Can't remove $file"
+#      exit 1
+#    fi
+#  fi
+#done
 
-for file in nextcloud/apps/*; do
-  if [ ! "$file" = "nextcloud/apps/index.php" ]; then
-    rm -rf "$file"
-    if [ $? ]; then
-      echo "Removed $file"
-    else
-      echo "Can't remove $file"
-      exit 1
-    fi
-  fi
-done
+#for file in nextcloud/apps/*; do
+#  if [ ! "$file" = "nextcloud/apps/index.php" ]; then
+#    rm -rf "$file"
+#    if [ $? ]; then
+#      echo "Removed $file"
+#    else
+#      echo "Can't remove $file"
+#      exit 1
+#    fi
+#  fi
+#done
