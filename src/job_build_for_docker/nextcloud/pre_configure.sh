65119b48e8f534ef3a6fc32081dddf246fd6889a
HyperPaint <dioman172@yandex.ru>
2023-09-03 14:10:13 +1000

    Удаление не требуется

--- src/job_build_for_docker/nextcloud/pre_configure.sh
+++ src/job_build_for_docker/nextcloud/pre_configure.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Удалять не нужно, т.к. нет тем
 #for file in nextcloud/themes/*; do
 #  if [ ! "$file" = "nextcloud/themes/index.php" ]; then
 #    rm -rf "$file"
@@ -12,6 +13,7 @@
 #  fi
 #done
 
+# Удалять не нужно, т.к. плагины в поставке не предназначены для удаления
 #for file in nextcloud/apps/*; do
 #  if [ ! "$file" = "nextcloud/apps/index.php" ]; then
 #    rm -rf "$file"
